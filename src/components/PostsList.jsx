7340081179debb51d32003b34649b5f15e19da8e
hikurt <32220244+hikurt@users.noreply.github.com>
2023-03-28 15:41:45 +0200

    Added modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,40 @@
+import React, { useState } from 'react';
+
+import NewPost from './NewPost';
 import Post from "./Post";
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [ enteredBody, setEnteredBody ] = useState('');
+  const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
+  const hideModalHandler = () => {
+    setModalIsVisible(prevValue => !prevValue);
+  };
+
   return (
-    <ul className={classes.posts}>
-      <Post author='Max' body='Nice react brah'/>
-      <Post author='Loki' body='What a Vue'/>
+    <React.Fragment>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+        </Modal>
+       ) : null }
+      <ul className={classes.posts}>
+      <Post author={enteredAuthor} body={enteredBody} />
+      <Post author={enteredAuthor} body={enteredBody} />
     </ul>
+    
+    </React.Fragment>
     
   );
 }
